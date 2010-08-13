fb7e4a6f40a553bb36d979d1e5d8798a0126005f
Brett Graham <graham@rowland.harvard.edu>
2010-08-13 16:24:39 -0400

    updated test functions for rayfitting algorithm, looks like we need at the very least 4 points per ray

--- electrodeController/rayfit.py
+++ electrodeController/rayfit.py
@@ -10,8 +10,6 @@ from mpl_toolkits.mplot3d import Axes3D
 
 import vector #bjg.vector
 
-debugPlotting = False#True
-
 def draw_circle(ax, center, normal, radius):
     r = radius
     c = center
@@ -28,7 +26,7 @@ def draw_circle(ax, center, normal, radius):
     ax.plot(pts[:,0],pts[:,1],pts[:,2])
 
 
-def measure_rotation_plane(pts, angles, radii):
+def measure_rotation_plane(pts, angles, radii, debugPlotting=False):
     pts = pts[:,:3] # remove extra dimension from homogeneous imputs
     # measure normal [mn]
     xyzs = pts - mean(pts, 0)
@@ -57,12 +55,12 @@ def measure_rotation_plane(pts, angles, radii):
         gd[ai,ri] = p
     
     if debugPlotting:
-        print tpts
-        print gd
-    
-        print tpts[0], tpts[2]
-        print gd[0,0], gd[0,1]
-    
+        # print tpts
+        # print gd
+        #     
+        # print tpts[0], tpts[2]
+        # print gd[0,0], gd[0,1]
+        
         figure()
         for (i,p) in enumerate(tpts):
             text(p[0],p[1],i)
@@ -96,21 +94,37 @@ def measure_rotation_plane(pts, angles, radii):
     mrs = zeros(len(radii))
     for i in xrange(len(radii)):
         mrs[i] = sqrt(sum((pts[i] - mc)**2))
-
+    
     return mc, mn, mrs
 
 
-if __name__ == '__main__':
-    import bjg.cfgBase as cfg
+def generate_fake_data(n, c, radii, angles, noise=0.):
+    n = n / norm(n)
+    if sum((n - array([0,0,1]))**2) < 0.001:
+        u = cross(n,array([0,1,0]))
+    else:
+        u = cross(n,array([0,0,1]))
+    v = cross(n,u)
+    u = u / norm(u)
+    v = v / norm(v)
+    
+    pts = array([c + r*cos(a)*u + r*sin(a)*v for r,a in zip(radii, angles)])
+    pts += (rand(*pts.shape) - 0.5) * noise
+    return pts
 
+def basic_test():
+    import bjg.cfgBase as cfg
+    
     cfg.random = False
     cfg.verbose = False
     cfg.plot = True
     cfg.noise = 0.0 # peak-to-peak
     cfg.err = 0.001 # acceptable error [mm]
     cfg.dataFile = None
+    cfg.nAngles = 2
+    cfg.nRadii = 3
     cfg.process_command_line()
-
+    
     if cfg.verbose:
         logging.basicConfig(level=logging.DEBUG)
     else:
@@ -133,20 +147,21 @@ if __name__ == '__main__':
         pts = d[:,4:7]
         angles = d[:,7]
         radii = d[:,8]
-    
-        mc, mn, mrs = measure_rotation_plane(pts, angles, radii)
+        
+        mc, mn, mrs = measure_rotation_plane(pts, angles, radii, cfg.plot)
         print "Center:", mc
         print "Normal:", mn
         print "Radii:", mrs
         print
         print "Radius", median(mrs - radii)
-    
+        
         # for plottings
         c = mc
         n = mn
-    
+        
         mr1 = median((mrs[0],mrs[1]))
         mr2 = median((mrs[2],mrs[3]))
+        mr3 = median((mrs[4],mrs[5]))
     else:
         # generate fake data (4 points, 2 at radius r1 at angles a1 and a2, 2 at radius r2 at a1 and a2)
         if cfg.random:
@@ -155,103 +170,37 @@ if __name__ == '__main__':
         else:
             c = array([0,0,0]) # center of rotation
             n = array([0,1,0]) # rotation plane normal
-
+        
         n = n / norm(n)
-        u = cross(n,array([0,0,1])) # vector to a point on the circle
-        v = cross(n,u)
-        u = u / norm(u)
-        v = v / norm(v)
-        r1 = 90.
-        r2 = 91.
-        r3 = 92.
-        a1 = radians(40.)
-        a2 = radians(45.)
-        radii = array([r1, r1, r2, r2, r3, r3])
-        angles = array([a1, a2, a1, a2, a1, a2])
+        
+        radii = arange(cfg.nRadii) + 90.
+        angles = arange(cfg.nAngles) + 40.
+        radii = transpose(vstack([radii]*cfg.nAngles)).flatten()
+        angles = hstack([angles]*cfg.nRadii)
+        # r1 = 90.
+        # r2 = 91.
+        # r3 = 92.
+        # a1 = radians(40.)
+        # a2 = radians(45.)
+        # radii = array([r1, r1, r2, r2, r3, r3])
+        # angles = array([a1, a2, a1, a2, a1, a2])
         #pts = c + radii*cos(angles)*u + radii*sin(angles)*v
-        pts = array([c + r*cos(a)*u + r*sin(a)*v for r,a, in zip(radii,angles)])
-        # pts = array([ c + r1*cos(a1)*u + r1*sin(a1)*v,
-        #             c + r1*cos(a2)*u + r1*sin(a2)*v,
-        #             c + r2*cos(a1)*u + r2*sin(a1)*v,
-        #             c + r2*cos(a2)*u + r2*sin(a2)*v])
-        # add noise
-        pts += (rand(*pts.shape) - 0.5) * cfg.noise
-
-        mc, mn, mrs = measure_rotation_plane(pts, angles, radii)
-
+        pts = generate_fake_data(n,c,radii,angles,cfg.noise)
+        
+        mc, mn, mrs = measure_rotation_plane(pts, angles, radii, cfg.plot)
+        
         mr1 = median((mrs[0],mrs[1]))
         mr2 = median((mrs[2],mrs[3]))
         mr3 = median((mrs[4],mrs[5]))
-
+        
         logit('Normal', n, mn)
         logit('Center(3d)', c, mc)
-        logit('R1', r1, mr1)
-        logit('R2', r2, mr2)
-        logit('R3', r3, mr3)
-
-
-    # # -------------- Normal: measure normal of points --------------------
-    # xyzs = pts - mean(pts,0)
-    # u, s, vh = svd(xyzs)
-    # i = s.argmin()
-    # mn = vh[i]
-    # mn = mn/norm(mn)
-    # logit('Normal', n, mn)
-    # 
-    # 
-    # # -------------- Project onto Plane -----------------------------------
-    # # TODO there has to be a better way to do this, brain is not working
-    # u = cross(mn, array([0,0,1]))
-    # v = cross(mn, u)
-    # u = u / norm(u)
-    # v = v / norm(v)
-    # P = vector.rebase(u, v, mn)
-    # v1 = u
-    # v2 = v
-    # v3 = mn
-    # 
-    # # ss = sort(s)[::-1]
-    # # v1 = vh[list(s).index(ss[0])]
-    # # v2 = vh[list(s).index(ss[1])]
-    # # v3 = vh[list(s).index(ss[2])]
-    # 
-    # P = vector.rebase(v1/norm(v1), v2/norm(v2), v3/norm(v3))
-    # hpts = vector.make_homogeneous(xyzs)
-    # tpts = vector.apply_matrix_to_points(P,hpts)[:,:2]
-    # 
-    # 
-    # # --------------- Find Ray Intersection --------------------------------
-    # m1 = (tpts[2,1] - tpts[0,1])/(tpts[2,0] - tpts[0,0])
-    # m2 = (tpts[3,1] - tpts[1,1])/(tpts[3,0] - tpts[1,0])
-    # b1 = tpts[2,1] - m1 * tpts[2,0]
-    # b2 = tpts[3,1] - m2 * tpts[3,0]
-    # ix = (b2 - b1)/(m1 - m2)
-    # iy = m1*ix + b1
-    # 
-    # 
-    # # ------------------ Reproject center to 3d ----------------------
-    # mc = array(vector.apply_matrix_to_points(inv(P),array([[ix,iy,0.,1.]])))[0][:3] + mean(pts,0)
-    # logit('Center(3d)', c, mc)
-    # 
-    # 
-    # # --------------- Measure Radius ----------------------------------------
-    # ds = sqrt(sum((pts-mc)**2,1))
-    # mr1a, mr1b, mr2a, mr2b = ds
-    # # mr1a = sqrt((tpts[0,0] - ix)**2 + (tpts[0,1] - iy)**2)
-    # # mr1b = sqrt((tpts[1,0] - ix)**2 + (tpts[1,1] - iy)**2)
-    # # mr1a = sqrt(sum((tpts[0] - (ix,iy))**2))
-    # # mr1b = sqrt(sum((tpts[1] - (ix,iy))**2))
-    # mr1 = median((mr1a,mr1b))
-    # logit('R1', r1, mr1)
-    # 
-    # # mr2a = sqrt((tpts[2,0] - ix)**2 + (tpts[2,1] - iy)**2)
-    # # mr2b = sqrt((tpts[3,0] - ix)**2 + (tpts[3,1] - iy)**2)
-    # # mr2a = sqrt(sum((tpts[2] - (ix,iy))**2))
-    # # mr2b = sqrt(sum((tpts[3] - (ix,iy))**2))
-    # mr2 = median((mr2a,mr2b))
-    # logit('R2', r2, mr2)
-
-
+        # logit('R', radii, mrs)
+        logit('R', 0., median(mrs - radii))
+        # logit('R1', r1, mr1)
+        # logit('R2', r2, mr2)
+        # logit('R3', r3, mr3)
+    
     # --------------------- Plotting -----------------------------------
     if cfg.plot:
         ax = Axes3D(figure())
@@ -281,16 +230,97 @@ if __name__ == '__main__':
         ax.set_ylim3d(yl)
         ax.set_zlim3d(zl)
         
-        
         ax.set_xlim3d(oxl)
         ax.set_ylim3d(oyl)
         ax.set_zlim3d(ozl)
         
-        
-        # figure()
-        # scatter(tpts[:,0],tpts[:,1],c='b')
-        # scatter(ix,iy,c='r')
-        # plot([tpts[2,0],ix],[tpts[2,1],iy],c='g')
-        # plot([tpts[3,0],ix],[tpts[3,1],iy],c='g')
-        
         show()
+
+def plot_error():
+    import bjg.cfgBase as cfg
+    
+    cfg.random = False
+    cfg.verbose = False
+    cfg.plot = True
+    cfg.noise = 0.0 # peak-to-peak
+    cfg.err = 0.001 # acceptable error [mm]
+    cfg.dataFile = None
+    cfg.nAngles = 2
+    cfg.nRadii = 3
+    cfg.nReps = 10
+    cfg.process_command_line()
+    
+    radii = arange(cfg.nRadii) + 2.
+    
+    errs = []
+    for r in radii:
+        errs.append([])
+        for i in xrange(cfg.nReps):
+            errs[-1] += list(test_error(r,cfg.noise))
+    figure()
+    for (i,err) in enumerate(errs):
+        subplot(221)
+        scatter([radii[i]]*len(err),err)
+        subplot(222)
+        scatter(radii[i],median(err))
+        subplot(223)
+        scatter(radii[i],std(err))
+        subplot(224)
+        scatter(radii[i],mean(err))
+    subplot(221)
+    title('error')
+    xlabel('radii used')
+    ylabel('error (actual-measured)')
+    subplot(222)
+    title('median')
+    subplot(223)
+    title('std')
+    subplot(224)
+    title('mean')
+    
+    # errs = zeros((cfg.nReps,cfg.nRadii*cfg.nAngles))
+    # for i in xrange(len(errs)):
+    #     errs[i] = test_error(cfg.nRadii,cfg.noise)
+    # 
+    # for (i,err) in enumerate(errs):
+    #     scatter([i]*len(err),err)
+    # xlabel('repetitions')
+    # ylabel('error (actual-measured)')
+    
+    # radii = arange(3) + 3
+    # noise = arange(0.0,0.1,0.001)
+    # errGrid = zeros((len(radii),len(noise)))
+    # for (i,r) in enumerate(radii):
+    #     for (j,n) in enumerate(noise):
+    #         errGrid[i,j] = sum(test_error(r,n))
+    # #imshow(errGrid)
+    # c = contourf(noise,radii,errGrid)
+    # colorbar(c)
+    
+    show()
+
+def test_error(nRadii, noise):
+    c = (rand(3) - 0.5)*1000.0
+    n = (rand(3) - 0.5)
+    n = n / norm(n)
+    
+    #nRadii = 3
+    nAngles = 2
+    radii = arange(nRadii) + 90.
+    angles = arange(nAngles) * 1. + 40.
+    radii = transpose(vstack([radii]*nAngles)).flatten()
+    angles = hstack([angles]*nRadii)
+    
+    pts = generate_fake_data(n,c,radii,angles,noise)
+    
+    mc, mn, mrs = measure_rotation_plane(pts, angles, radii, False)
+    
+    # logit('Normal', n, mn)
+    # logit('Center(3d)', c, mc)
+    
+    return (radii - mrs)
+
+if __name__ == '__main__':
+    #basic_test()
+    plot_error()
+    
