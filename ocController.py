c43da3ac2106938e5753b9a32f0d4e6112b33e45
Lab User <labuser@coxlabphysiology1.rowland.org>
2010-08-30 11:41:38 -0400

    fixing nstimer

--- ocController.py
+++ ocController.py
@@ -113,14 +113,11 @@ class OCController (NSObject, electrodeController.controller.Controller):
     
     def applicationWillTerminate_(self, sender):
         print "applicationWillTerminate called"
-        self.timer.invalidate()
+        #self.timer.invalidate()
         self.cleanup()
         #self.cameras.disconnect()
-<<<<<<< HEAD
         #self.timer.invalidate()
         self.stop_update_timer()
-=======
->>>>>>> 5e4e875c8b57b70d1077375fb0b40428709047cc
         print "applicationWillTerminate done"
         
     
