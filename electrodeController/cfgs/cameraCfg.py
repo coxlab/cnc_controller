e9f4d1912e72b6077691c4c574ef6a4e98c5fc79
Lab User <labuser@coxlabphysiology1.rowland.org>
2010-10-06 17:23:24 -0400

    changed grid size

--- electrodeController/cfgs/cameraCfg.py
+++ electrodeController/cfgs/cameraCfg.py
@@ -6,7 +6,7 @@ import logging
 # ----- Camera Calibration Settings -----
 # ordered as 'left', 'right'
 camIDs = [49712223528793951, 49712223528793946]
-gridSize = (7,6)
+gridSize = (8,6)#(7,6)
 gridBlockSize = 1.
 
 # TODO sort out where this should go
