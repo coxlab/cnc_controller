4d0395a45c05b5d4b8994c480d0ad23266b84a2b
David Cox <david.daniel.cox@gmail.com>
2010-10-27 15:18:53 -0400

    basically working-ish version of electrode finder

--- electrode_finder/electrode_finder.py
+++ electrode_finder/electrode_finder.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 from image_proc import canny, hough_line_segments, array2cv
-from scipy.ndimage import binary_erosion, imread, map_coordinates, spline_filter, median_filter
+from scipy.ndimage import binary_erosion, imread, map_coordinates, spline_filter, median_filter, gaussian_filter, watershed_ift
 from scipy import convolve
 import scipy.optimize
 import matplotlib.pylab as plt
@@ -11,6 +11,12 @@ from numpy.linalg import norm
 from scipy.interpolate import interp2d
 
 plot_alot = False
+no_optimization = False
+
+resampling_size = 0.25
+background_region_size = 25  # for estimating "background" mean and std
+tip_threshold_multiplier = 10
+slab_oversample = 0.65
 
 def find_electrode(im):
     # apply some sort of windowing / exclude irrelvant image pixels
@@ -63,7 +69,7 @@ def find_electrode(im):
     return best_line
 
 
-def resample_slab(im, line, sample_width=20, sample_step=1, over_factor=1.5):
+def resample_slab(im, line, sample_width=20, sample_step=0.25, over_factor=1.5):
     
     e_start = array(line[0])
     e_end = array(line[1])
@@ -108,9 +114,9 @@ def resample_slab(im, line, sample_width=20, sample_step=1, over_factor=1.5):
     if plot_alot:
         plt.imshow(im)
         plt.hold(True)
-        for i in range(0, X_im.shape[0], 4):
-            for j in range(0, X_im.shape[1], 4):
-                plt.plot(X_im[i,j], Y_im[i,j], '*');
+        #for i in range(0, X_im.shape[0], 4):
+        #    for j in range(0, X_im.shape[1], 4):
+        #        plt.plot(X_im[i,j], Y_im[i,j], '*');
         plt.colorbar()
         plt.show()
     
@@ -120,7 +126,7 @@ def resample_slab(im, line, sample_width=20, sample_step=1, over_factor=1.5):
     transformed.shape = X_im.shape
     
     # Create a transform to get back to image coordinates
-    return_transform = lambda c: e_center + c[1] * e_dir + (c[0] - sample_width) * e_ortho
+    return_transform = lambda c: e_center + (c[1]*sample_step) * e_dir + ((c[0]*sample_step) - sample_width) * e_ortho
     
     return (transformed, return_transform)
 
@@ -133,16 +139,21 @@ def estimate_tip_location_in_slab(slab):
     cross_section = mean(slab, 0)
     center_axis = argmin(cross_section)
     trough_depth = min(cross_section)
+
     
     width = 3
-    flank_width = 5
+    flank_width = 2
     center_slice = mean(slab[:, center_axis-width:center_axis+width], 1) 
     left_flank = mean(slab[:, 0:center_axis-flank_width], 1)
     right_flank = mean(slab[:, center_axis+flank_width:], 1)
     
     
     normalized_center_slice = center_slice -  (left_flank + right_flank)/2.
-   
+    
+    # smoothing_width = 3
+    #     k = ones(smoothing_width,'d')
+    #     x = convolve(k/k.sum(),normalized_center_slice)
+    #     normalized_center_slice = x[0:len(normalized_center_slice)]
     
     
     if plot_alot:
@@ -152,14 +163,15 @@ def estimate_tip_location_in_slab(slab):
     
     
     # determine the noise level for the background
-    background_offset = 10
+    background_offset = background_region_size
     background_std = std(normalized_center_slice[-background_offset:])
+    normalized_center_slice = normalized_center_slice - mean(normalized_center_slice[-background_offset:])
     
     tip_cutoff = -1
     for t in range(background_offset, len(normalized_center_slice)):
         
         val = normalized_center_slice[-t]
-        if val < -4 * background_std:
+        if val < -tip_threshold_multiplier * background_std:
             tip_cutoff = len(normalized_center_slice) - t
             break
     
@@ -175,11 +187,13 @@ def estimate_tip_location_in_slab(slab):
     return (array([center_axis,tip_cutoff]), trough_depth)
 
 
-def electrode_alignment_objective(params, im, line_segment):
+def electrode_alignment_objective(params, im, line_segment, **kwargs):
     try:
         a = params[0]
     except:
         a = params
+    
+    sampling_step = kwargs.pop("sampling_step", 0.25)
     
     print "Evaluating angle = ", a
     theta = a * pi / 180.
@@ -196,7 +210,12 @@ def electrode_alignment_objective(params, im, line_segment):
     
     line_segment = (e_start, e_start + new_dir)
     
-    (slab, to_image_coords) = resample_slab(im, line_segment, 20, 1, 0.75)
+    (slab, to_image_coords) = resample_slab(im, line_segment, 20, sampling_step, slab_oversample)
+    
+    use_watershed = False # doesn't work correctly  
+    if use_watershed:
+        slab = transform_electrode_image_watershed(slab, None, [int(slab.shape[0]/2.), int(slab.shape[1]/2.)])
+    
     (tip, trough) = estimate_tip_location_in_slab(slab)
     
     if plot_alot:
@@ -205,7 +224,7 @@ def electrode_alignment_objective(params, im, line_segment):
         plt.plot(tip[0], tip[1], 'r*')
         plt.show()
     
-    return (-trough, to_image_coords(tip))
+    return (trough, to_image_coords(tip))
 
 def find_electrode_tip(im, line_segment):
     
@@ -225,16 +244,16 @@ def find_electrode_tip(im, line_segment):
 def find_electrode_tip_with_refinement(im, base_im, angle_range, angle_resolution):
     
     if base_im is not None:
-        diff_im = abs(im - base_im)
+        diff_im = im - base_im
     else:
         diff_im = im
         print "WARNING: no base (sans electrode) image provided"
     
-    diff_im = median_filter(diff_im, 4)
+    #diff_im = median_filter(diff_im, 4)
     
     l = find_electrode(diff_im)
     tip = find_electrode_tip(diff_im, l)
-    
+    a = [angle_range[0]]
     a = scipy.optimize.brute(lambda a: electrode_alignment_objective(a, diff_im, l)[0], ranges=(slice(angle_range[0],angle_range[1],angle_resolution),))
     print "a=", a
     (trough, new_tip) = electrode_alignment_objective(a[0], diff_im, l)
@@ -242,21 +261,53 @@ def find_electrode_tip_with_refinement(im, base_im, angle_range, angle_resolutio
     return new_tip
     
 
-def find_electrode_tip_from_segment(im, base_im, l, angle_range, angle_resolution):
+def find_electrode_tip_from_segment(im, base_im, l, angle_range, nsteps):
 
+    im = im - mean(im.ravel())
+    im = im / std(im.ravel())
     if base_im is not None:
-        diff_im = abs(im - base_im)
+        base_im = base_im - mean(base_im.ravel())
+        base_im = base_im / std(base_im.ravel())
+        diff_im = im - base_im
     else:
         diff_im = im
         print "WARNING: no base (sans electrode) image provided"
 
     diff_im = median_filter(diff_im, 4)
 
-    a = scipy.optimize.brute(lambda a: electrode_alignment_objective(a, diff_im, l)[0], ranges=(slice(angle_range[0],angle_range[1],angle_resolution),))
-    print "a=", a
-    (trough, new_tip) = electrode_alignment_objective(a[0], diff_im, l)
+    #diff_im = transform_electrode_image_watershed(im, base_im, l[0])
+    a = [angle_range[0]]
+    
+    if not no_optimization:
+        ranges = [ (angle_range) ]
+        print ranges
+        angle = scipy.optimize.brute(lambda a_: electrode_alignment_objective(a_, diff_im, l, sampling_step=resampling_size)[0], ranges, (), nsteps, 0, None)
+    print "angle=", angle
+    (trough, new_tip) = electrode_alignment_objective(angle, diff_im, l)
 
     return new_tip
+
+
+# THIS DOES NOT WORK CORRECTLY
+def transform_electrode_image_watershed(im, base_im, seed_point):
+    if base_im is not None:
+        diff_im = im - base_im
+    else:
+        diff_im = im
+        print "WARNING: no base (sans electrode) image provided"
+
+    diff_im = median_filter(diff_im, 4)
+
+    input_im = im.astype(uint16)
+    
+    #return input_im
+    markers = zeros_like(input_im)
+    markers[0,0] = 0
+    markers[seed_point[0], seed_point[1]] = 1
+    
+    res = watershed_ift(input_im, markers.astype(int))
+    
+    return double(res)
 
 
 if __name__ == "__main__":
@@ -275,17 +326,19 @@ if __name__ == "__main__":
     
     if True:
         
-        for filename in ("1287516647", "1287516595", "1287516711"):
-            base_dir = "/Users/davidcox/Desktop/1287516173/cameras/49712223528793946/"
-            #base_im = double(imread(base_dir + "1287516595.png"))
-            base_im = None
+        for filename in ("1288193037","1288193138", "1288193185", "1288192975",):
+            base_dir = "/Volumes/vnsl/cncLogs/1288191881/cameras/49712223528793946/"
+            base_im = double(imread(base_dir + "1288192514" + ".png"))
+            #base_im = gaussian_filter(base_im, 20.)
+            #base_im = None
             im = double(imread(base_dir + filename + ".png"))
-            est_tip = [608., 527.]
-            est_shank = [551., 442.]
+            est_tip = [606., 546.]
+            #est_tip = [620., 540.]
+            est_shank = [558., 470.]
         
             segment = [est_shank, est_tip]
         
-            tip = find_electrode_tip_from_segment(im, base_im, segment, (-1,0), 1)
+            tip = find_electrode_tip_from_segment(im, base_im, segment, (-0.5,0.5), 20)
     
     
             plt.imshow(im, cmap=cm.gray)
